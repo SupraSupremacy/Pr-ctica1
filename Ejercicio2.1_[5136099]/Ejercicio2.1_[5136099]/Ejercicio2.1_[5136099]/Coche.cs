209fa208482299e4a9527b9b8a28e47dc61d9c92
Jesús <djesus.aleman.m@gmail.com>
2025-02-20 19:15:15 -0600

    Add files via upload

---
+++ Ejercicio2.1_[5136099]/Ejercicio2.1_[5136099]/Ejercicio2.1_[5136099]/Coche.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._1__5136099_
+{
+    public class Coche
+    {
+        public Coche(string marca, string modelo, int velocidadMax, int velocidadAct)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.VelocidadMax = velocidadMax;
+            this.VelocidadAct = velocidadAct;
+        }
+        public string Marca;
+        public string Modelo;
+        public int VelocidadMax;
+        public int VelocidadAct;
+
+        public void Acelerar(int velocidad)
+        {
+            Console.WriteLine("Acelerando " + velocidad  ," km/h");
+        }
+        public void Desacelerar(int velocidad)
+        {
+            Console.WriteLine("desacelerando " + velocidad , " km/h");
+            this.VelocidadAct = velocidad - 5;
+        }
+    }
+}
+//marca, modelo, velocidad máxima, velocidad actual, acelera (de 5 en 5), decelera (de 5 en 5)
---
+++ Ejercicio2.1_[5136099]/Ejercicio2.1_[5136099]/Ejercicio2.1_[5136099]/Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._1__5136099_.Properties
+{
+   public class Program
+    {
+        static void Main(string[] args)
+            {
+                Coche Coche1 = new Coche("Mazda", "MX-5 Miata NA", 195, 100);
+                Console.WriteLine("Descripción del vehículo:");
+                Console.WriteLine("Marca: " + Coche1.Marca);
+                Console.WriteLine("Modelo: " + Coche1.Modelo);
+                Console.WriteLine("Velocidad Max: " + Coche1.VelocidadMax);
+                Console.WriteLine("Velocidad Act: " + Coche1.VelocidadAct);
+                Coche1.Acelerar(5);
+                Coche1.Desacelerar(5);
+            }
+    }
+}
