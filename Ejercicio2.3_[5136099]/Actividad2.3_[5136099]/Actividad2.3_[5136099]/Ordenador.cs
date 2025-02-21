2ea85bce519fc9244d33ecc83bb2d901c029475b
Jesús <djesus.aleman.m@gmail.com>
2025-02-20 19:17:15 -0600

    Add files via upload

---
+++ Ejercicio2.3_[5136099]/Actividad2.3_[5136099]/Actividad2.3_[5136099]/Ordenador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2._3__5136099_
+{
+    public class Ordenador
+    {
+        public Ordenador(string marca, string procesador, string memoria, string discoduro)
+        {
+            this.Marca = marca;
+            this.Procesador = procesador;
+            this.Memoria = memoria;
+            this.DiscoDuro = discoduro;
+        }
+        public string Marca;
+        public string Procesador;
+        public string Memoria;
+        public string DiscoDuro;
+
+        public void MayorCapacidad(int capacidad)
+        {
+            Console.WriteLine("La capacidad del disco duro ha sido aumentada " + capacidad + "GB");
+        }
+        public void MenorCapacidad(int capacidad)
+        {
+            Console.WriteLine("La capacidad del disco duro ha sido reducida " + capacidad + "GB");
+        }
+    }
+}
---
+++ Ejercicio2.3_[5136099]/Actividad2.3_[5136099]/Actividad2.3_[5136099]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad2._3__5136099_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ordenador Ordenador1 = new Ordenador("HP", "Intel(R) Core(TM) i7-3520M CPU 2.90GHz 2.90 GHz", "12.0GB DDR3", "KINGSTON SA400S37480G ATA Device");
+            Console.WriteLine("Descripción del ordenador:");
+            Console.WriteLine("Marca: " + Ordenador1.Marca);
+            Console.WriteLine("Procesador: " + Ordenador1.Procesador);
+            Console.WriteLine("Memoria: " + Ordenador1.Memoria);
+            Console.WriteLine("Disco Duro: " + Ordenador1.DiscoDuro);
+            Ordenador1.MayorCapacidad(100);
+            Ordenador1.MenorCapacidad(100);
+        }
+    }
+}
